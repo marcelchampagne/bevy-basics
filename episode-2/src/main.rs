0f095a659098622687fe3fab1e1882ab09ed10fc
Marcel Champagne <voiceofmarcel@gmail.com>
2023-10-25 12:16:28 -0700

    Episode 2 - Bundles, Plugins, Cameras, 3D models

--- episode-2/src/main.rs
+++ episode-2/src/main.rs
@@ -1,39 +1,95 @@
 use bevy::prelude::*;
 
 #[derive(Component, Debug)]
-struct Position {
-    x: f32,
-    y: f32,
+struct Velocity {
+    value: Vec3,
 }
 
-#[derive(Component, Debug)]
-struct Velocity {
-    x: f32,
-    y: f32,
+struct MovementPlugin;
+
+impl Plugin for MovementPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, update_position);
+    }
+}
+
+struct DebugPlugin;
+
+impl Plugin for DebugPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, print_position);
+    }
+}
+
+#[derive(Bundle)]
+struct SpaceshipBundle {
+    velocity: Velocity,
+    model: SceneBundle,
+}
+
+struct SpaceshipPlugin;
+
+impl Plugin for SpaceshipPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_spaceship);
+    }
+}
+
+struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_camera);
+    }
 }
 
 fn main() {
     App::new()
-        .add_systems(Startup, spawn_spaceship)
-        .add_systems(Update, (update_position, print_position))
+        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 0.5,
+        })
+        .add_plugins(MovementPlugin)
+        .add_plugins(DebugPlugin)
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(CameraPlugin)
         .add_plugins(DefaultPlugins)
         .run();
 }
 
-fn spawn_spaceship(mut commands: Commands) {
-    commands.spawn((Position { x: 0.0, y: 0.0 }, Velocity { x: 1.0, y: 1.0 }));
+fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(SpaceshipBundle {
+        velocity: Velocity {
+            value: Vec3::new(0.0, 0.0, 1.0),
+        },
+        model: SceneBundle {
+            scene: asset_server.load("Spaceship.glb#Scene0"),
+            transform: Transform::from_xyz(0.0, 0.0, -20.0),
+            ..default()
+        },
+    });
 }
 
-fn update_position(mut query: Query<(&Velocity, &mut Position)>) {
-    for (velocity, mut position) in query.iter_mut() {
-        position.x += velocity.x;
-        position.y += velocity.y;
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 80.0, 0.0).looking_at(Vec3::ZERO, Vec3::Z),
+        ..default()
+    });
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.translation += velocity.value * time.delta_seconds();
     }
 }
 
-fn print_position(query: Query<(Entity, &Position)>) {
-    // Log the entity ID and position of each entity with a `Position` component.
-    for (entity, position) in query.iter() {
-        info!("Entity {:?} is at position {:?},", entity, position);
+fn print_position(query: Query<(Entity, &Transform)>) {
+    // Log the entity ID and translation of each entity with a `Position` component.
+    for (entity, transform) in query.iter() {
+        info!(
+            "Entity {:?} is at position {:?},",
+            entity, transform.translation
+        );
     }
 }
