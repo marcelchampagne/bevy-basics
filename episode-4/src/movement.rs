4d45f3553a6748a498938b88fcd934dc0263249d
Thu Nguyen <awesomethuna@gmail.com>
2023-11-24 19:31:27 -0800

    Episode 4 - Ordering Systems

---
+++ episode-4/src/movement.rs
@@ -0,0 +1,58 @@
+use bevy::prelude::*;
+
+use crate::{collision_detection::Collider, schedule::InGameSet};
+
+#[derive(Component, Debug)]
+pub struct Velocity {
+    pub value: Vec3,
+}
+
+impl Velocity {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+#[derive(Component, Debug)]
+pub struct Acceleration {
+    pub value: Vec3,
+}
+
+impl Acceleration {
+    pub fn new(value: Vec3) -> Self {
+        Self { value }
+    }
+}
+
+#[derive(Bundle)]
+pub struct MovingObjectBundle {
+    pub velocity: Velocity,
+    pub acceleration: Acceleration,
+    pub collider: Collider,
+    pub model: SceneBundle,
+}
+
+pub struct MovementPlugin;
+
+impl Plugin for MovementPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            (update_velocity, update_position)
+                .chain()
+                .in_set(InGameSet::EntityUpdates),
+        );
+    }
+}
+
+fn update_velocity(mut query: Query<(&Acceleration, &mut Velocity)>, time: Res<Time>) {
+    for (acceleration, mut velocity) in query.iter_mut() {
+        velocity.value += acceleration.value * time.delta_seconds();
+    }
+}
+
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
+    for (velocity, mut transform) in query.iter_mut() {
+        transform.translation += velocity.value * time.delta_seconds();
+    }
+}
